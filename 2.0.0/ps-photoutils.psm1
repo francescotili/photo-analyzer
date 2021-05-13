99e12d55fabf070fa02933e69424c2e06e88c0a7
Francesco Tili <francesco.tili@hotmail.it>
2021-05-13 17:09:37 +0200

    Bug fix and minor fix

--- 2.0.0/ps-photoutils.psm1
+++ 2.0.0/ps-photoutils.psm1
@@ -2,16 +2,17 @@ $global:WorkingFolder = ""
 $global:ScriptMode = ""
 
 # PRIVATE FUNCTIONS
-. $PSScriptRoot\private\RenameFile.ps1
+. $PSScriptRoot\private\AnalyzeFiles.ps1
 . $PSScriptRoot\private\ChangeExtension.ps1
-. $PSScriptRoot\private\ParseDateTime.ps1
-. $PSScriptRoot\private\IsValidDate.ps1
 . $PSScriptRoot\private\CheckFileType.ps1
 . $PSScriptRoot\private\Helpers.ps1
+. $PSScriptRoot\private\IsValidDate.ps1
 . $PSScriptRoot\private\Outputs.ps1
+. $PSScriptRoot\private\ParseDateTime.ps1
+. $PSScriptRoot\private\RenameFile.ps1
 
 # PUBLIC FUNCTIONS
-. $PSScriptRoot\public\GetExifInfo.ps1
-. $PSScriptRoot\public\WriteExifInfo.ps1
 . $PSScriptRoot\public\CleanBackups.ps1
+. $PSScriptRoot\public\GetExifInfo.ps1
 . $PSScriptRoot\public\PhotoAnalyzer.ps1 # Main
+. $PSScriptRoot\public\WriteExifInfo.ps1
