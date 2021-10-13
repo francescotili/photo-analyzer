ca30077562b2989c7bd402a3e6f96af112d92369
Francesco Tili <francesco.tili@hotmail.it>
2021-10-13 18:28:04 +0200

    Added match check in function

--- 2.0.0/private/ParseFilename.ps1
+++ 2.0.0/private/ParseFilename.ps1
@@ -16,19 +16,29 @@ Function ParseFilename {
     [String]$filename
   )
   $pattern = "(19|20\d{2})(?:[_.-])?(0[1-9]|1[0-2])(?:[_.-])?([0-2]\d|3[0-1]).*([0-1][0-9]|2[0-3])(?:[_.-])?([0-5][0-9])(?:[_.-])?([0-5][0-9])"
-  
-  # Match the filename with pattern
-  $regMatches = [regex]::Matches($filename, $pattern)
 
-  # Identify the capture groups
-  $year = $regMatches.Groups[1].Value
-  $month = $regMatches.Groups[2].Value
-  $day = $regMatches.Groups[3].Value
-  $hour = $regMatches.Groups[4].Value
-  $minute = $regMatches.Groups[5].Value
-  $second = $regMatches.Groups[6].Value
+  # Check if filename is parsable
+  if ( $filename -match $pattern ) { # We have a match
+    # Match the filename with pattern
+    $regMatches = [regex]::Matches($filename, $pattern)
 
-  # Return date in format "YYYY:MM:dd HH:mm:ss"
-  $returnDate = "$($year):$($month):$($day) $($hour):$($minute):$($second)"
-  return $returnDate
+    # Identify the capture groups
+    $year = $regMatches.Groups[1].Value
+    $month = $regMatches.Groups[2].Value
+    $day = $regMatches.Groups[3].Value
+    $hour = $regMatches.Groups[4].Value
+    $minute = $regMatches.Groups[5].Value
+    $second = $regMatches.Groups[6].Value
+
+    # Return date in format "YYYY:MM:dd HH:mm:ss"
+    $returnDate = "$($year):$($month):$($day) $($hour):$($minute):$($second)"
+
+    if ( IsValidDate $returnDate ) { # Valid parsed date
+      return $returnDate
+    } else {
+      return ""
+    }
+  } else { # Parsing not possible
+    return ""
+  }
 }
