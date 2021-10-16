222fa2730df2694691ea77dae45f9481796ff6b5
Francesco Tili <francesco.tili@hotmail.it>
2021-10-16 10:06:22 +0200

    Confirmation removed

--- 2.0.0/public/PhotoAnalyzerAuto.ps1
+++ 2.0.0/public/PhotoAnalyzerAuto.ps1
@@ -10,22 +10,8 @@ Function PhotoAnalyzerAuto {
   # Ask for path
   Set-Path
 
-  # Show selected path
   Clear-Host
-  OutputScriptHeader
-  OutputModeStatus
-  OutputWorkingPath
-  Write-Host ""
-  $userChoice = Read-Host ">> Do you want to continue? y/n"
-
-  switch ($userChoice) {
-    'y' { # Execute main actions
-      Clear-Host
-      OutputSpacer
-      AutoAnalyzeFiles
-      OutputScriptFooter
-    }
-    Default {
-      Read-Host -Prompt "Press Enter to exit"}
-  }
+  OutputSpacer
+  AutoAnalyzeFiles
+  OutputScriptFooter
 }
